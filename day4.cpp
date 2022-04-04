fafe0058e3d0d44e12fad69331719c3d7e158c33
bajihuo <3405235455@qq.com>
2022-04-04 22:20:16 +0800

    day4

---
+++ day4.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>;
+
+
+//初识函数
+
+int Add(int x, int y) {
+	int z = 0;
+	z = x + y;
+	return z;
+}
+int main() {
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d%d", &num1, &num2);
+	int sum = Add(num1, num2);
+
+	printf("%d\n", sum);
+}
+
+//数组
+
+char ch[5] = { '1','2','3','4','5' };//初始化
+char ch2[7] = { '1','2','3' };//不完全初始化，未初始化部分默认为0
