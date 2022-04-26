83141e6b0ba3ca93341e415b5de474d20667cb6c
bajihuo <3405235455@qq.com>
2022-04-26 22:10:19 +0800

    day26

--- day26.c
+++ day26.c
@@ -1,9 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+
+/*
 //goto  关机小程序
 int main() {
-
 	char input[20] = { 0 };
 	system("shutdown -s -t 120");
 	printf("%s", "《pig一号程序》\n!\n骇客入侵\n请在两分钟内输入指令：我是猪，否则电脑将会关机!（打完要回车）\n");
@@ -17,6 +18,14 @@ begin:
 		printf("%s", "不要妄图抵抗！\n");
 		goto begin;
 	}
-
 }
+*/
 
+//函数
+// 
+//替换内存中的字节
+int main(){
+	char arr[] = { "hello world" };
+	memset(arr, 'h', 5);//要替换的内存地址，要替换的内容，要替换的范围
+	printf("%s", arr);
+}
