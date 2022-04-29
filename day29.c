d95eea252e2e41c2584eca9c9429a23137ebff02
bajihuo <3405235455@qq.com>
2022-04-29 22:03:20 +0800

    day29

--- day29.c
+++ day29.c
@@ -2,3 +2,23 @@
 #include <stdio.h>
 
 //º¯ÊýµÝ¹é
+
+void print(unsigned int i) {
+	if (i > 10) {
+		print(i / 10);
+	}
+	printf("%d\n", i % 10);
+
+
+}
+
+int main() {
+
+	unsigned int num = 0;
+	scanf("%u", &num);
+	print(num);
+
+
+}
+
+
